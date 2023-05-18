88096906808bef53dcbbd341c589c3ba94f180a2
donaldk12345 <fotsoyvesdonald@gmail.com>
2023-05-18 19:40:26 +0200

    d

--- src/component/list-todo.js
+++ src/component/list-todo.js
@@ -1,14 +1,30 @@
 import { useEffect, useState } from "react";
 import http from "../http-common";
 
-export default function ListTodo() {
+const ListesTodos = () => {
 
-    const [todos, setTodos] = useState([]);
+    const [Todos, setTodos] = useState([]);
 
      function listTodos() {
-        http.get("/todo").then((data) => {
-            console.log(data);
-            setTodos(data);
+        http.get("/todo").then((response) => {
+            console.log(response.data);
+            setTodos(response.data);
+
+            const dat = response.data;
+        
+
+        }).catch((error) => {
+            console.log(error);
+        })
+    }
+    function deleteTodo(id) {
+  
+         http.delete("/todo/" + id).then((response) => {
+            console.log(response.data);
+            setTodos(response.data);
+
+            const dat = response.data;
+        
 
         }).catch((error) => {
             console.log(error);
@@ -21,11 +37,18 @@ export default function ListTodo() {
 
     <div className="w-full px-20 bg-primary-light py-14">
            <div className="flex flex-col items-center max-w-sm mx-auto">
-               <h3 className="max-w-2xl text-2xl font-bold leading-tight text-center sm:text-3xl md:text-2xl md:leading-tight">Liste des todos</h3><br />
-               {
-                   todos.map(item => (
-                       <span> {item.name} </span>
-                   ))
+               <h3 className="max-w-2xl text-2xl font-bold leading-tight text-center sm:text-3xl md:text-2xl md:leading-tight">Liste des todos</h3>
+                 
+                {
+               Todos.map((item) => 
+                     
+               (
+                            <span key={item.id} className="m-2">
+                               <small>{item.name}</small> &nbsp;&nbsp;<button className="px-2 py-2 font-bold text-white rounded-lg  bg-red-600" onClick={deleteTodo}>Delete</button>
+                           </span>
+                  )
+                       
+                   )
                   }
                
            </div>
@@ -33,3 +56,5 @@ export default function ListTodo() {
   );
     
 }
+
+export default ListesTodos;
